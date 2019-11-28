2658e1289b9a0e7b9b82aa36aa21cf7d85ad05cf
xiekind <ziekind07@gmail.com>
2019-11-28 12:06:14 +0800

    initial

--- src/admin/AdminSignUp.js
+++ src/admin/AdminSignUp.js
@@ -5,78 +5,49 @@ class AdminSignUp extends Component{
     constructor(props){
         super(props);
         this.state = {
-            name: "",
-            email:"",
-            pass:"",
-            phone:"",
+            username: "",
+            password: "",
             state: false
         }
     }
 
-    nameHandler(e){
-        this.setState({name: e.target.value})
-    }
-
-    emailHandler(e){
-        this.setState({email: e.target.value})
-    }
-
-    passwordHandler(e){
-        this.setState({pass: e.target.value})
-    }
-
-    phoneHandler(e){
-        this.setState({phone: e.target.value})
-    }
-
-    registerHandler(e) {
-        if(this.state.name === ""){
-            this.setState({state:false})
-            swal("Aww snap!", "The name is required", "error");
-        }
-        
-        if(this.state.email === ""){
-            this.setState({state:false})
-            swal("Aww snap!", "The email is required", "error");
-        }
-        if(this.state.pass === ""){
-            this.setState({state:false})
-            swal("Aww snap!", "The code is required", "error");
-        }
-        if(this.state.phone === ""){
-            this.setState({state:false})
-            swal("Aww snap!", "The phone is required", "error");
-        }
-        else{
-            this.setState({state:true})
-        }
-    }
+    loginHandler = () => {
+        event.preventDefault();
+        var body = {username: this.state.username, password: this.state.password};
+        req
+        .login(body)
+        .then(res => {
+            if(this.state.username === "administrator" && this.state.password === "admin") {
+                this.setState({state:true})
+            }
+            else{
+                alert(res.data.sms);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    };
 
     render() {
-        if(this.state.state === false)
-        {
-            return(
-                <center id="admin">
-                    <br/>
-                    <h2>Fill up the form</h2>
-                    Name:<br/>
-                    <input placeholder="name" onChange={(e) => this.nameHandler(e)}></input><br/>
-                    Email:<br/>
-                    <input placeholder="email" onChange={(e) => this.emailHandler(e)}></input><br/>
-                    Password:<br/>
-                    <input type="password" placeholder="password" onChange={(e) => this.passwordHandler(e)}></input><br/>
-                    Phone:<br/>
-                    <input placeholder="phone" onChange={(e) => this.phoneHandler(e)}></input><br/>
-                    <button onClick={(e) => this.registerHandler(e)}>SignUp</button>
+        if(this.state.state) {
+            return <Redirect to = "/adminForm"/>;
+        }
 
+        return (
+            <div>
+                <center>
+                    Username:<br/>
+                    <input type="text" value="username" /><br/>
+                    Password:<br/>
+                    <input type="password" value="password"/><br/>
+                    <button onClick={(e) => this.loginHandler(e)}>SUBMIT</button>
                 </center>
-            )
-        }
-        else
-        {
-             
-        }
+            </div>
+        )
     }
+
+
 }
 
 export default AdminSignUp;
